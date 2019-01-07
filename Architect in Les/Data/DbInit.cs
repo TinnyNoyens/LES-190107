4a0eaeb64fa080d069de21aa7f94e575cf8a7791
Tinny Noyens <c1040397@student.thomasmore.be>
2019-01-07 19:31:29 +0100

    Add project files.

---
+++ Architect in Les/Data/DbInit.cs
@@ -0,0 +1,29 @@
+﻿using Les190107.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Les190107.Data
+{
+    public class DbInit
+    {
+        public static void Initialize(CustomerContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //Zoek customers
+            if (context.Customers.Any())
+            {
+                return;
+            }
+
+            context.Customers.AddRange(
+                new Customer { Voornaam ="Tinny", Naam="Noyens"},
+                new Customer { Voornaam ="Willem", Naam="Snoeckx"}
+                );
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Architect in Les/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Les190107.Models
+{
+    public class Customer
+    {
+
+        public int ID { get; set; }
+
+        public string Voornaam { get; set; }
+
+        public string Naam { get; set; }
+    }
+}
